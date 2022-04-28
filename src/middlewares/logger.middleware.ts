f37edcff166edc5ec8234bd7bc5bd8e42b11d3cc
Mohab Alnajjar <mohabsoft21@outlook.com>
2022-04-28 03:05:53 +0200

    Logger Middleware

---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,7 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+export function logger(req: Request, res: Response, next: NextFunction) {
+  console.log(`${req.url}/${req.method}`);
+  next();
+}
